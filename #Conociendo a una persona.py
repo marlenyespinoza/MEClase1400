e93b8c954466a0cdd5814dca5fb99a1b4d7b319f
marlenyespinoza <marlenyespinoza@mail.weber.edu>
2025-09-18 19:11:07 -0600

    agregando archivo del saludo

---
+++ #Conociendo a una persona.py
@@ -0,0 +1,14 @@
+#Conociendo a una persona 
+nombre= input ("Hola como te llamas?")
+print("Mucho gusto" + nombre)
+#Pregunta por su estado de animo
+estadodeanimo = input ("Como estas bien o mal?")
+if estadodeanimo == "Bien":
+    print("😊")
+elif estadodeanimo == "Mal":
+    print("😢")
+else:
+    print("😤")
+#Preguntar por su pais 
+pais = input ("De donde eres?")
+print("Ese lugar es bonito")         
